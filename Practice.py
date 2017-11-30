d2c4b6d9ebc4302614e62a568980f0c3cdc74e80
vcpavao <korgkeys13>
2017-11-29 22:21:23 -0500

    Updated / renamed file
    Renamed file
    
    First elements of statistics

--- Practice.py
+++ Practice.py
@@ -20,22 +20,28 @@ Selects random clue from database
 """
 def select_clue(conn):
     incorrect_ids = []
+    total = 0
+    correct = 0
     cur = conn.cursor()
     while True: # selects a random row from database
         cur.execute("SELECT * FROM documents ORDER BY RANDOM() LIMIT 1;")
         clue_id, clue, answer = cur.fetchone()
-        category = cur.execute("SELECT category FROM categories WHERE id = ? LIMIT 1;", (clue_id,))
+        category = cur.execute("SELECT category FROM classifications, categories WHERE id = category_id and clue_id = ?;", (clue_id,))
         category = cur.fetchone()
         print()
         print("CATEGORY: ", category)
         print(clue)
         # compare user answer with actual answer
         usr_answer = input()
-        if usr_answer != answer:
-            print(answer)
-            incorrect_ids.append(clue_id)
-        else:
+        if usr_answer.lower() in answer.lower() and len(usr_answer) > 1 :
             print("Correct!")
+            correct += 1
+        else :
+            print("ANSWER: ", answer)
+            incorrect_ids.append(clue_id)
+        total += 1
+        pct = 100 * correct/total
+        print(pct, "%")
         
  
  
@@ -49,10 +55,11 @@ def main():
     with conn:           
         # Select clues from database
         select_clue(conn)
-        
-        # If correct, forget about it, add result to statistics
-        # If incorrect, mark it as incorrect, add result to statistics
-        # Statistics = % of questions answered right in category
+        """
+        If correct, forget about it, add result to statistics
+        If incorrect, mark it as incorrect, add result to statistics
+        Statistics = % of questions answered right in category
+        """
 
 if __name__ == '__main__':
     main()
